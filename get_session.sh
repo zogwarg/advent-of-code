01493c74871bedfad047e55ae63180f8c1a4e6c2
Thomas Buick <thomas.buick@gmail.com>
2023-12-04 19:03:32 +0900

    Improve chrome cookie query

--- get_session.sh
+++ get_session.sh
@@ -3,9 +3,8 @@
 # Get session from chrome cookies on mac, using hombrew openssl
 if [[ -n $CHROME_PWD ]]; then
 sqlite3 ~/Library/Application\ Support/Google/Chrome/Default/Cookies \
-  'SELECT encrypted_value FROM cookies where host_key = ".adventofcode.com" and name = "session"' \
-  | xxd -ps | tr -d '\n' \
-  | jq -Rrs '.[6:-2]' | xxd -r -ps \
+  'SELECT hex(substr(encrypted_value,4)) FROM cookies where host_key = ".adventofcode.com" and name = "session"' \
+  | xxd -r -ps \
   | openssl enc -d -aes-128-cbc \
       -iv 20202020202020202020202020202020 -K $(
         openssl kdf -keylen 16 -kdfopt iter:1003 -kdfopt salt:saltysalt -kdfopt pass:${CHROME_PWD} PBKDF2 | tr -d :
